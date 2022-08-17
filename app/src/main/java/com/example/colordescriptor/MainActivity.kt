70cbdd9f386850fd171732ec72154a8ef3e13b9f
Vitaliy Radugin <vitalyran@mail.ru>
2022-08-17 03:15:45 +0300

    Realised app

--- app/src/main/java/com/example/colordescriptor/MainActivity.kt
+++ app/src/main/java/com/example/colordescriptor/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.colordescriptor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.view.get
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+
+    private lateinit var spinnerColors: Spinner
+    private lateinit var textViewDescriptionTemp: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        spinnerColors = findViewById(R.id.spinnerColors)
+        textViewDescriptionTemp = findViewById(R.id.textViewDscriptionTemp)
+    }
+
+    fun showDescription(view: View) {
+        val position : Int = spinnerColors.selectedItemPosition
+        val description : String = getDescriptionByPosition(position)
+        textViewDescriptionTemp.text = description
+    }
+
+    private fun getDescriptionByPosition (position : Int): String {
+        val descriptions : List<String> = resources.getStringArray(R.array.description_of_temp).toList()
+        return descriptions[position]
     }
 }
